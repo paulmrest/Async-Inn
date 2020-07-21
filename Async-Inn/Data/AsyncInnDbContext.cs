26bbe668529770b09de5e03ae425e1a3ecd7b7c0
paulmrest <paulmrest@gmail.com>
2020-07-21 14:19:06 -0700

    Setup From Class

---
+++ Async-Inn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Async_Inn.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Hotel Corona",
+                    StreetAddress = "123 PPE Lane",
+                    City = "Nada",
+                    State = "UB",
+                    Phone = "(555) 555-5555"
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Hotel Corona 2",
+                    StreetAddress = "321 PPE Lane",
+                    City = "Nada",
+                    State = "UB",
+                    Phone = "(555) 555-5555"
+                }
+            );
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
---
+++ Async-Inn/Migrations/20200721190643_SeedDataTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Async_Inn.Migrations
+{
+    public partial class SeedDataTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "City", "Name", "Phone", "State", "StreetAddress" },
+                values: new object[] { 1, "Nada", "Hotel Corona", "(555) 555-5555", "UB", "123 PPE Lane" });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "City", "Name", "Phone", "State", "StreetAddress" },
+                values: new object[] { 2, "Nada", "Hotel Corona 2", "(555) 555-5555", "UB", "321 PPE Lane" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
