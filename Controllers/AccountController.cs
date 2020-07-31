0ce05d9e8c1d51810f691fa9af222e8cf19a1cea
paulmrest <paulmrest@gmail.com>
2020-07-30 17:20:55 -0700

    Role/Policy auth working

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
 using System.Security.Claims;
 using System.Text;
@@ -14,6 +15,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using SQLitePCL;
 
 namespace Async_Inn.Controllers
 {
@@ -37,6 +39,11 @@ namespace Async_Inn.Controllers
         //api/account/register
         [Authorize(Policy = "DistrictAndPropertyManagers")]
         [HttpPost, Route("register")]
+        //[Authorize(Roles= "District Manager")]
+        //[Authorize(Roles = ApplicationRoles.DistrictManager)]
+        //[Authorize(Roles = "District Manager, Property Manager")]
+        //[Authorize(Policy= "DistrictAndPropertyManagers")]
+        //[AllowAnonymous]
         public async Task<IActionResult> Register(RegisterDTO registerDTO)
         {
             ApplicationUser user = new ApplicationUser()
@@ -49,9 +56,9 @@ namespace Async_Inn.Controllers
             var result = await _userManager.CreateAsync(user, registerDTO.Password);
             if (result.Succeeded)
             {
-                if (user.Email == _config["DistrictManagerSeed"])
+                if (registerDTO.Role != null)
                 {
-                    await _userManager.AddToRoleAsync(user, ApplicationRoles.DistrictManager);
+                    await _userManager.AddToRoleAsync(user, GetRole(registerDTO));
                 }
                 await _signInManager.SignInAsync(user, false);
                 return Ok();
@@ -94,8 +101,8 @@ namespace Async_Inn.Controllers
             {
                 var user = await _userManager.FindByEmailAsync(loginDTO.Email);
 
-                //var identityRole = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Role);
-                var token = CreateToken(user);
+                var identityRole = await _userManager.GetRolesAsync(user);
+                var token = CreateToken(user, identityRole.ToList());
 
                 return Ok(new
                 {
@@ -106,21 +113,47 @@ namespace Async_Inn.Controllers
             return BadRequest("Invalid attempt");
         }
 
-        private JwtSecurityToken CreateToken(ApplicationUser user)
+        private string GetRole(RegisterDTO registerDTO)
         {
-            var authClaims = new[]
+            string role = "";
+            switch (registerDTO.Role.ToLower())
+            {
+                case "district manager":
+                    role = ApplicationRoles.DistrictManager;
+                    break;
+                case "property manager":
+                    role = ApplicationRoles.PropertyManager;
+                    break;
+                case "agent":
+                    role = ApplicationRoles.Agent;
+                    break;
+                default:
+                    break;
+            }
+            return role;
+        }
+
+        private JwtSecurityToken CreateToken(ApplicationUser user, List<string> roles)
+        {
+            var authClaims = new List<Claim>()
             {
                 new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                 new Claim("FirstName", user.FirstName),
                 new Claim("LastName", user.LastName),
-                new Claim("UserId", user.Id),
+                new Claim("UserId", user.Id)
             };
+
+            foreach (var role in roles)
+            {
+                authClaims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
             var token = AuthenticateToken(authClaims);
             return token;
         }
 
-        private JwtSecurityToken AuthenticateToken(Claim[] cliams)
+        private JwtSecurityToken AuthenticateToken(List<Claim> claims)
         {
             var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWTKey"]));
             var token = new JwtSecurityToken
@@ -128,7 +161,7 @@ namespace Async_Inn.Controllers
                 issuer: _config["JWTIssuer"],
                 audience: _config["JWTIssuer"],
                 expires: DateTime.UtcNow.AddHours(24),
-                claims: cliams,
+                claims: claims,
                 signingCredentials: new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256)
             );
             return token;
